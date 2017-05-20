adbe9e96cb04e741b4c52289b6178b7212ad950e
blackhatv6 <bhaveshbhatia85@gmail.com>
2017-05-20 08:48:03 +0530

    Add files via upload

---
+++ Printing text.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+cout<<"Hi"<<"\t";
+Sleep(2000);
+cout<<"How"<<"\t";
+Sleep(2000);
+cout<<"are"<<"\t";
+Sleep(2000);
+cout<<"you ?";
+return 0;
+}
